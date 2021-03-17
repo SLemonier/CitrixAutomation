f3974220c8970c738e09a6608ac1c220fd09d81e
Steven Lemonier <mail@stevenlemonier.fr>
2021-03-17 09:56:28 +0100

    Check DeliveryGroup parameter

--- Morning_Report.ps1
+++ Morning_Report.ps1
@@ -5,24 +5,37 @@
 .DESCRIPTION
   Check all PCS XenApp servers are up and running at 8:00.
   Email report to Citrix engineers.
+.Parameter Dev
+ During script development, send the report to the "Dev" recipients list.
+.Parameter DeliveryController
+ Specifiy the Delivery Controller to use for the provision
+ This parameter is optionnal, by default it will use with the local machine.
 .EXAMPLE
     PS C:\PSScript > Morning_Report.ps1 
     Run some checks during the morning and ensure the Citrix infrastructure is compliamt with our tresholds (check Description for more details).
+.EXAMPLE
+    PS C:\PSScript > Morning_Report.ps1 -DeliveryController "CTXDDC01"
+    Run some checks during the morning and ensure the Citrix infrastructure is compliamt with our tresholds (check Description for more details).
+.EXAMPLE
+    PS C:\PSScript > Morning_Report.ps1 -Dev
+    Run some checks during the morning and ensure the Citrix infrastructure is compliamt with our tresholds (check Description for more details),
+    then send the mail to the "Dev" recipients list (to avoid spam during script development).
 .INPUTS
     None. You cannot pipe objects to this script.
 .OUTPUTS
     No objects are output from this script. This script creates its own logs files.
 .NOTES
-  Version:        0.1
+  Version:        0.2
   Author:         Steven Lemonier
   Creation Date:  2021-03-08
 #>
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$false)] [switch]$Dev,$
+    [Parameter(Mandatory=$false)] [switch]$Dev,
     [Parameter(Mandatory=$false)] [string]$DeliveryController,
-    [Parameter(Position = 1, Mandatory=$False)][Alias("OF")][ValidateNotNullOrEmpty()] [string]$OutFilePath="C:\Temp\Morning_Report.log"
+    [Parameter(Mandatory=$false)] [string[]]$DeliveryGroup,
+    [Parameter(Mandatory=$False)][Alias("OF")][ValidateNotNullOrEmpty()] [string]$OutFilePath="C:\Temp\Morning_Report.log"
 )
 
 Start-Transcript -Path $OutFilePath -Append
@@ -63,18 +76,41 @@ if(($DDC)){
     Write-Host "Cannot contact the Delivery Controller. Please, check the role is installed on the target computer and your account is allowed to communicate with it." -ForegroundColor Red
 }
 
+#Check if the DeliveryGroup(s) specified in parameter exist(s)
+if($DeliveryGroup){
+    foreach($DG in $DeliveryGroup){
+        Write-Host "Checking if $DG exists..." -NoNewline
+        try {
+            Get-BrokerDesktopGroup -AdminAddress $DeliveryController -Name $DG | Out-Null
+            Write-host "OK" -ForeGroundColor Green
+        }
+        catch {
+            Write-host "Failed" -ForeGroundColor Red
+            "$DG is not a valid DeliveryGroup. Please, check the parameter then restart the script" -ForeGroundColor Red
+            Stop-Transcript
+            exit
+        }
+    }
+}
+
+Stop-Transcript
+exit
+
 ###################################################################################################################
 # Mail settings
 ###################################################################################################################
 
 # E-mail report details
 $emailFrom = "citrix@hospicegeneral.ch"
-$emailTo = "Steven.Lemonier@hospicegeneral.ch", "Cedric.Bosson@hospicegeneral.ch"
+if($Dev){
+    $emailTo = "steven.lemonier@hospicegeneral.ch"
+} else {
+    $emailTo = "Steven.Lemonier@hospicegeneral.ch", "Cedric.Bosson@hospicegeneral.ch"
+}
 $smtpServer    = "smtp.implisis.ch"
 
 $mailbody = $mailbody + "<!DOCTYPE html>"
 $mailbody = $mailbody + "<html>"
-
 $mailbody = $mailbody + "<head>"
 $mailbody = $mailbody + "<style>"
 $mailbody = $mailbody + "BODY{background-color:#fbfbfb; font-family: Arial;}"
@@ -83,7 +119,6 @@ $mailbody = $mailbody + "TH{border-width: 1px;padding: 0px;border-style: solid;b
 $mailbody = $mailbody + "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;}"
 $mailbody = $mailbody + "</style>"
 $mailbody = $mailbody + "</head>"
-
 $mailbody = $mailbody + "<body>"
 
 
@@ -93,23 +128,23 @@ $mailbody = $mailbody + "<body>"
 
 $error = 0
 
-$PCSServers = Get-BrokerMachine -DesktopGroupName "W12 XenApp PCS 11-2" | Select MachineName,PowerState,InMaintenanceMode,RegistrationState
+$PCSServers = Get-BrokerMachine -AdminAddress $DeliveryController -DesktopGroupName  | Select MachineName,PowerState,InMaintenanceMode,RegistrationState
 foreach($server in $PCSServers){
     $MachineName = $server.MachineName
     if($server.Powerstate -ne "On"){
         $error++
         $mailbody += "<div style=""color:orange;"">$MachineName is not powered On! Script is powering On the server...</div>"
-        New-BrokerHostingPowerAction -MachineName $MachineName -Action TurnOn
+        New-BrokerHostingPowerAction -AdminAddress $DeliveryController -MachineName $MachineName -Action TurnOn
         Start-Sleep -Seconds 120
-        if((Get-BrokerMachine -MachineName $MachineName).PowerState -ne "On" -and (Get-BrokerMachine -MachineName $MachineName).RegistrationState -ne "Registered"){
+        if((Get-BrokerMachine -AdminAddress $DeliveryController -MachineName $MachineName).PowerState -ne "On" -and (Get-BrokerMachine -MachineName $MachineName).RegistrationState -ne "Registered"){
             $mailbody += "<div style=""color:red;"">Cannot start and register $MachineName. Please check the server manually.</div>"
         }
     }
     if($server.inMaintenanceMode -eq $true){
         $error++
         $mailbody += "<div style=""color:orange;"">$MachineName is in Maintenance Mode! Script is disabling maintenance mode...<br/></div>"
-        Get-BrokerMachine -MachineName $MachineName | Set-BrokerMachine -InMaintenanceMode $false
-        if((Get-BrokerMachine -MachineName $MachineName).InMaintenanceMode -eq $true){
+        Get-BrokerMachine -AdminAddress $DeliveryController -MachineName $MachineName | Set-BrokerMachine -InMaintenanceMode $false
+        if((Get-BrokerMachine -AdminAddress $DeliveryController -MachineName $MachineName).InMaintenanceMode -eq $true){
             $mailbody += "<div style=""color:red;"">Cannot exit Maintenance Mode on $MachineName. Please check the server manually.</div><br/>"
         } else {
             $mailbody += "$MachineName is now out of Maintenance Mode.<br/>"
@@ -132,6 +167,6 @@ $mailbody = $mailbody + "</body>"
 $mailbody = $mailbody + "</html>"
  
 
-Send-MailMessage -to $emailTo -from $emailFrom -subject "Hospice General Citrix Morning Report" -Body $mailbody -BodyAsHtml -SmtpServer $smtpServer
+Send-MailMessage -to $emailTo -from $emailFrom -subject "Citrix Morning Report" -Body $mailbody -BodyAsHtml -SmtpServer $smtpServer
 
 Stop-Transcript
